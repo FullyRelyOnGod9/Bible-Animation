31e74752b79eefe562312eb51e447124f47cdf79
Dmitriy Trokhimenko <dmitriy04tr@gmail.com>
2022-01-19 15:11:44 +0200

    Added DVD-Animation

---
+++ src/Panel.java
@@ -0,0 +1,58 @@
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Panel extends JPanel implements ActionListener {
+
+	final int WIDTH = 700;
+	final int HEIGHT = 700;
+	Image enemy;
+	Timer timer;
+	int xVelocity = 1;
+	int yVelocity = 1;
+	int x = 0;
+	int y = 0;
+	
+	Panel(){
+		
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.setBackground(Color.black);
+		enemy = new ImageIcon("img/dvd0.png").getImage();
+		timer = new Timer(10, this);
+		timer.start();
+	}
+	
+	public void paint(Graphics g) {
+		
+		super.paint(g);
+		
+		Graphics2D g2D = (Graphics2D) g;
+		g2D.drawImage(enemy, x, y, null);
+	}
+	
+	public void getRandomEnemy() {
+			
+		int randEnemy = (int) (Math.random() * 8);
+		enemy = new ImageIcon("img/dvd" + randEnemy + ".png").getImage();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		if(x >= WIDTH - enemy.getWidth(null) || x < 0) {
+			xVelocity = xVelocity * -1;
+			getRandomEnemy();
+		}
+		x = x + xVelocity;
+		
+		if(y >= HEIGHT - enemy.getHeight(null) || y < 0) {
+			yVelocity = yVelocity * -1;
+			getRandomEnemy();
+		}
+		y = y + yVelocity;
+		
+		repaint();
+	}	
+}
